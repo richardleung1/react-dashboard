061909dbc47aab9abe4e695af665dd7523db1493
richardleung1 <richardl57549@gmail.com>
2022-08-17 15:33:09 -0400

    Add components and style

--- app.js
+++ app.js
@@ -1,4 +1,99 @@
-ReactDOM.render(
-  <h1>Commence Dashboard Creation!</h1>,
-  document.querySelector("main")
-);
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div class="sidebar">
+        <ul class="sidelist">
+          <li>
+            <h3>Dashboard</h3>
+          </li>
+          <li>
+            <h3>Widget</h3>
+          </li>
+          <li>
+            <h3>Reviews</h3>
+          </li>
+          <li>
+            <h3>Customers</h3>
+          </li>
+          <li>
+            <h3>Online Analysis</h3>
+          </li>
+          <li>
+            <h3>Settings</h3>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Reviews extends React.Component {
+  render() {
+    return (
+      <div class="reviews">
+        <h2>Reviews</h2>
+        <h1>1281</h1>
+      </div>
+    );
+  }
+}
+
+class AverageRating extends React.Component {
+  render() {
+    return (
+      <div class="rating">
+        <h2>Average Rating</h2>
+        <h1>4.6</h1>
+      </div>
+    );
+  }
+}
+
+class SentimentAnalysis extends React.Component {
+  render() {
+    return (
+      <div class="analysis">
+        <h2>Sentiment Analysis</h2>
+        <ul>
+          <li>
+            <h2>960</h2>
+          </li>
+          <li>
+            <h2>122</h2>
+          </li>
+          <li>
+            <h2>321</h2>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class WebsiteVisitors extends React.Component {
+  render() {
+    return (
+      <div class="visitors">
+        <h2>Website Visitors</h2>
+        <h1>821</h1>
+        <div class="box"></div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <Sidebar />
+        <Reviews />
+        <AverageRating />
+        <SentimentAnalysis />
+        <WebsiteVisitors />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("main"));
